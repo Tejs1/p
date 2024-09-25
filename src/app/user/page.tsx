49fbe9885af11a387c208d3976b03a86d0978666
Tejs1 <tejsrelax@gmail.com>
2024-09-25 18:50:33 +0530

    setup iu

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,11 +1,11 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function HomePage() {
+export default function UserPage() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-center gap-2">
-      <h1 className="text-5xl">hello</h1>
-      <Link href="/user" className="text-5xl underline">
+      <h1 className="text-5xl">User</h1>
+      <Link href="/" className="text-5xl underline">
         <Button>User</Button>
       </Link>
     </main>
